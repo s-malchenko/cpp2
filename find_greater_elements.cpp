0b73b45ea0509a6b028c54d894e34aa525203362
s-malchenko <s.malchenko@dreamkas.ru>
2018-10-18 11:28:06 +0300

    find_greater_elements done

---
+++ find_greater_elements.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <set>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+template <typename T>
+vector<T> FindGreaterElements(const set<T>& elements, const T& border)
+{
+    auto it = find_if(elements.begin(), elements.end(), [&border](const T &i) { return i > border; });
+    return {it, elements.end()};
+}
+
+int main()
+{
+    for (int x : FindGreaterElements(set<int> {1, 5, 7, 8}, 5))
+    {
+        cout << x << " ";
+    }
+    cout << endl;
+
+    string to_find = "Python";
+    cout << FindGreaterElements(set<string> {"C", "C++"}, to_find).size() << endl;
+    return 0;
+}
